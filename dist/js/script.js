83cab37650f1414e72d926e8e9f53fd8f38fe65c
varekaiwork <serhiisaviukwork@gmail.com>
2020-11-26 10:26:10 +0200

    finish

--- dist/js/script.js
+++ dist/js/script.js
@@ -58,6 +58,16 @@ rulleteBtn.addEventListener('click',()=>{
     }
   })
   
+  document.addEventListener('mousemove', parallax)
+  function parallax(e){
+    this.querySelectorAll('.layer').forEach(layer =>{
+      const speed = layer.getAttribute('data-speed')
+      const x = (window.innerWidth - e.pageX*speed)/100
+      const y = (window.innerWidth - e.pageY*speed)/100
+
+      layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+  }
 })
 
 
